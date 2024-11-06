7865110abc34413c0984e92a2a89e63a0218b5cf
sirflint07 <fajobimichael123@gmail.com>
2024-11-06 18:53:16 +0100

    changed to new loo and made it totally responsive

--- components/Homescreen.js
+++ components/Homescreen.js
@@ -11,28 +11,33 @@ const Homescreen = () => {
     {/*br tags included to give more spacing based on different devices */}
       <p className="max-sm:hidden"><br/><br/><br/><br /></p>
       <p className="max-sm:visible sm:hidden mb-4"><br/><br/><br/></p>
-      <p className='max-md:visible md:hidden'><br/></p>
+      {/* <p className='max-md:visible md:hidden'><br/></p> */}
 
         <p className="inter-normal max-sm:text-xxl max-sm:leading-7 max-sm:w-12/13 max-sm:text-wrap text-3xl w-2/3 tracking-normal leading-10 xl:text-balance  lg:text-pretty sm:w-10/12 max-md:mt-2 lg:text-justify lg:mt-3">Paris based studio specializing in production, publishing and artists representation. Studio Hiroshima is dedicated in bringing visionary projects to life and provides comprehensive support to artists in the arts and entertainment industry.</p>
-        <section  id='contact' className="flex justify-between lg:w-9/12 xl:w-10/12 mt-16 text-lg max-lg:flex-col max-sm:gap-8 max-sm:mt-8 max-lg:gap-10">
+        <section  id='contact' className="flex justify-between lg:w-9/12 xl:w-10/12 mt-16 text-lg flex-col max-sm:gap-8 max-sm:mt-8 max-lg:gap-10">
           <div className="">
-          <p className="text-lg tracking-wide mb-4 roboto-light xl:text-lg xl:font-normal max-sm:font-normal">PARIS</p>
+          <p className="text-lg tracking-wide mb-4 roboto-light xl:text-lg">PARIS</p>
           <div className='space-y-2'>
-            <p className="home-text xl:font-normal max-sm:font-normal">39 Rue du Borrégo</p>
-            <p className="home-text xl:font-normal max-sm:font-normal">75020 Paris</p>
-            <p className="home-text xl:font-normal max-sm:font-normal">+33 6 70 35 26 76</p>
-            <p className=" xl:font-normal max-sm:font-normal text-xl">hello@studiohiroshima.fr</p></div>
-            <div className="mt-14 space-y-2">
-              <p className="max-sm:text-lg text-xl roboto-light xl:text-lg xl:font-normal max-sm:font-normal">AGENT</p>
-              <p className="capitalize text-base roboto-regular tracking-wider underline underline-offset-2 max-sm:mt-1 mt-2 xl:text-xl xl:font-medium max-sm:font-normal">
-              <a href="https://www.linkedin.com/in/shivfr/?locale=en_US" target="_blank">
-              Edwin Bes</a>
+            <p className="home-text">39 Rue du Borrégo</p>
+            <p className="home-text ">75020 Paris</p>
+            <p className="home-text">+33 6 70 35 26 76</p>
+            <p className="text-xl subpixel-antialiased poppins-light">hello@studiohiroshima.fr</p></div>
+            <div className="mt-10 space-y-2">
+              <p className="text-lg tracking-wide mb-4 roboto-light xl:text-lg">AGENT</p>
+              <p className="home-text underline underline-offset-2">
+              Edwin Bes
+              </p>
+              <p className="text-xl subpixel-antialiased poppins-light">
+              
+              edwin@studiohiroshima.fr
               </p>
             </div>
           </div>
-          <div>
-            <p className="text-lg tracking-wider mb-4 roboto-light xl:text-lg xl:font-normal max-sm:font-normal">LINKEDIN</p>
-            <p className="text-xl xl:font-normal max-sm:font-normal "><a href='https://www.linkedin.com/company/studio-hiroshima/' target='_blank'>@studio-hiroshima</a></p>
+          <div className='mt-8'>
+            <p className="text-lg tracking-wide mb-4 roboto-light xl:text-lg"><a target='_blank' href='https://www.linkedin.com/company/studio-hiroshima/'>LINKEDIN</a></p>
+          </div>
+          <div className='mt-8'>
+            <p className="text-lg tracking-wide mb-4 roboto-light xl:text-lg"><a target='_blank' href='https://www.instagram.com/studiohiroshima/'>INSTAGRAM</a></p>
           </div>
         </section>
         <br /><br /><br />
--- components/Navbar.js
+++ components/Navbar.js
@@ -12,13 +12,13 @@ const Navbar = () => {
     setShowMenu(!showMenu)
   }
   return (
-    <nav className='mb-12 fixed z-10 w-full  max-sm:mx-auto bg-opacity-100 bg-whitish'>
-      <div className='flex justify-between w-12/13 mx-auto py-8 items-center max-sm:w-11/12 max-md:items-start max-md:mt-2'>
-        <div className='max-sm:w-4/6'><Image src='/img/nav-logo.png' alt='logo' width={255} height={255}
+    <nav className='mb-12 fixed z-10 w-full  max-sm:mx-auto bg-opacity-100 bg-transparent'>
+      <div className='flex justify-between w-12/13 mx-auto py-3 items-center max-sm:w-11/12 max-md:items-start max-md:mt-2'>
+        <div className='max-sm:w-4/6 max-sm:-ml-4 sm:-ml-6 md:-ml-6 lg:-ml-5 xl:-ml-6 2xl:-ml-8'><Image src='/img/logo.png' alt='logo' width={270} height={270}
           priority className='object-cover w-full h-auto'
         /></div>
         <ul className='flex max-md:flex-col gap-10 max-md:items-end max-md:w-2/6 max-md:gap-0 max-md:justify-center max-md:-mt-55'>
-        <li className='inter-thin text-base tracking-widest subpixel-antialiased  max-sm:text-xv visible md:hidden cursor-pointer' onClick={handleClick}>{showMenu ? 'CLOSE' : 'MENU'}</li>
+        <li className='inter-thin text-base tracking-widest subpixel-antialiased visible md:hidden cursor-pointer max-sm:pt-3 max-md:pt-5 max-md:text-[16.6px] max-sm:text-[15px]' onClick={handleClick}>{showMenu ? 'CLOSE' : 'MENU'}</li>
 
     {/* Mobile Device*/}
         {showMenu &&
@@ -36,8 +36,8 @@ const Navbar = () => {
         }
     {/* Mobile Device*/}
 
-        <li className='inter-thin text-base tracking-widest subpixel-antialiased max-sm:font-medium max-sm:text-sm max-md:hidden md:visible'><a href='#contact'>CONTACT</a></li>
-        <li className='inter-thin text-base tracking-widest subpixel-antialiased max-sm:font-medium max-sm:text-sm max-md:hidden md:visible text-black-30'>STUDIO HIROSHIMA</li>
+        <li className='inter-thin text-base tracking-widest subpixel-antialiased max-sm:font-medium max-sm:text-sm max-md:hidden md:visible pt-2 xl:text-[16.6px]'><a href='#contact'>CONTACT</a></li>
+        <li className='inter-thin text-base xl:text-[16.6px] tracking-widest subpixel-antialiased max-sm:font-medium max-sm:text-sm max-md:hidden md:visible text-black-30 pt-2'>STUDIO HIROSHIMA</li>
         
         </ul>
       </div>
