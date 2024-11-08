dee36ca9f5eb59560b77804e8d42a8cefb739f01
sirflint07 <fajobimichael123@gmail.com>
2024-11-09 00:29:56 +0100

    removed all comments

--- components/SplashScreen.js
+++ components/SplashScreen.js
@@ -29,37 +29,3 @@ export default function SplashScreen({ onVideoEnd }) {
     </div>
   );
 }
-
-
-// "use client";
-// import { useEffect, useState } from "react";
-
-// export default function SplashScreen({ onAnimationEnd }) {
-//   const [played, setPlayed] = useState(false); // Track if GIF has played
-
-//   useEffect(() => {
-//     // Only run the timer if GIF hasn't played yet
-//     if (!played) {
-//       const timer = setTimeout(() => {
-//         onAnimationEnd(); // Trigger redirect to homepage
-//         setPlayed(true); // Set played to true after first play
-//       }, 3000); // Adjust time based on GIF length
-
-//       // Cleanup timer when component unmounts
-//       return () => clearTimeout(timer);
-//     }
-//   }, [onAnimationEnd, played]);
-
-//   // If GIF has already played, don't render anything
-//   if (played) return null;
-
-//   return (
-//     <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-//       <img
-//         src="/video/logo-hiroshima.gif"
-//         alt="Splash Screen"
-//         className="w-full h-auto max-w-full object-cover aspect-video"
-//       />
-//     </div>
-//   );
-// }
